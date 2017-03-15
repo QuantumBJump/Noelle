1ad0258e9b0818817e4fca3a6009de5960161dda
Quinn Stevens <quantumjump451@gmail.com>
2017-03-15 17:20:36 +0000

    added command to join voice channel

---
+++ noelle.py
@@ -0,0 +1,43 @@
+import discord
+import asyncio
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+client = discord.Client()
+
+def get_channel_id(server, name):
+    for channel in server.channels:
+        if channel.name == name:
+            return channel.id
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('-------')
+
+@client.event
+async def on_message(message):
+    
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, "Calculating messages...")
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+    elif message.content.startswith('!join'):
+        channel_name = message.content.split()[1]
+        channel_to_join = client.get_channel(get_channel_id(message.server, channel_name))
+        await client.send_message(message.channel, 'Joining channel: ' + channel_name)
+        await client.join_voice_channel(channel_to_join)
+
+client.run('MjkxMjAxMzkzNDYzOTE4NTk0.C6mB_A.khU5NicYbB_1-erIA7jwd4b5ydM')
